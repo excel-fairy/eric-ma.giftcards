a2be91c6906cb09e82ba4d516c247c00246699d5
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-10-16 01:40:49 +0200

    chore: reverted startegy: giftcard will be appended from the distributor spreadsheet. It is either not possible or hard to call a GAS API from another GAS

--- sales-vps/add-giftcard.js
+++
@@ -1,7 +0,0 @@
-/**
- * Add a giftcard to the list
- * @param giftCard Object that has two fields: barcode and address
- */
-function addGiftCard(giftCard) {
-    GIFTCARDS_LIST_SHEET.sheet.appendRow([giftCard.barcode, giftCard.address, 'Available']);
-}
--- sales-vps/global-variables.js
+++
@@ -1,6 +0,0 @@
-var GIFTCARDS_LIST_SHEET = {
-    sheet: SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1"),
-    barcodeColumn: ColumnNames.letterToColumnStart0('A'),
-    addressColumn: ColumnNames.letterToColumnStart0('B'),
-    statusColumn: ColumnNames.letterToColumnStart0('C')
-};
