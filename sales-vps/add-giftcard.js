3144cadd50ffb047a5753fc8d06e02188412be79
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-10-16 01:04:54 +0200

    chore: documentation

--- sales-vps/add-giftcard.js
+++ sales-vps/add-giftcard.js
@@ -1,3 +1,7 @@
+/**
+ * Add a giftcard to the list
+ * @param giftCard Object that has two fields: barcode and address
+ */
 function addGiftCard(giftCard) {
     GIFTCARDS_LIST_SHEET.sheet.appendRow([giftCard.barcode, giftCard.address, 'Available']);
 }
